3ed80879fe9b42862a240938cf976f4b84be8d39
ajmal\Ajmal hassan <ajmalhassan651@gmail.com>
2018-09-07 14:34:19 +0500

    ecommerce landing page

---
+++ landing_page/home.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Landing Page</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+         <div id="top_bar">
+                    <ul>
+                        <li><a href="#">Help</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Delivery Information</a></li>
+                        <li><a href="#">Call us : 03011234567</a></li>
+                    </ul>
+                </div>
+        <div id="main-container">
+            <header>
+               
+                <div id="top_header">
+                    <img src="images/logo.png" alt="Online store">
+                    <ul>
+                        <li><a href="#">LOGIN</a></li>
+                        <li><a href="#">REGISTER</a></li>
+                        <li>My Cart(0): 0$ </li>
+                        <li><a style="color: #fff;" href="#">CHECKOUT</a></li>
+                    </ul>
+                    <div style="clear: both"></div>
+                    <form id="search_form" action="#" method="post" >
+                        <input type="text" name="search" placeholder="Search">
+                    </form>
+                    <div style="clear: both;"></div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">HOME</a></li>
+                            <li><a href="#">NEW ARRIVALS</a></li>
+                            <li><a href="#">TUXEDO</a></li>
+                            <li><a href="#">SWEATER</a></li>
+                            <li><a href="#">SHOES</a></li>
+                            <li><a href="#">GLASSES</a></li>
+                            <li><a href="#">T-SHIRT</a></li>
+                            <li><a href="#">WATCHES</a></li>
+                        </ul>
+                    </nav>
+                    <div id="header_bottom">
+                        <div id="new_arival">
+                            <h2>NEW ARRIVAL</h2>
+                            <P>REVIVE YOUR WARDROBE WITH CHIC KNITS</P>
+                            <a href="#">TAKE A LOOK</a>
+                        </div>
+                        <div id="tuxedo">
+                            <h2>tuxedo</h2>
+                            <P>REVIVE YOUR WARDROBE WITH CHIC KNITS</P>
+                            <a href="#">show now</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div id="sweater">
+                    <h2>sweater</h2>
+                    <p>REVIVE YOUR WARDROBE WITH CHIC KNITS</p>
+                    <a href="#">show now</a>
+                </div>
+                <div id="trekking_shoes">
+                    <h2>trekking shoes</h2>
+                </div>
+                <div class="main_bottom">
+                    <h2>casual glasses</h2>
+                </div>
+                <div class="main_bottom">
+                    <h2>fresh look t-shirt</h2>
+                    <a href="#">show now</a>
+                </div>
+                <div class="main_bottom">
+                     <h2>fresh look t-shirt</h2>
+                </div>
+                <div style="clear: both"></div>
+                <div id="offer">
+                    <h3>Special Offers</h3>
+                </div>
+                <div id="special_offer">
+                    <div class="offer-view">
+                        <img src="images/view1.png">
+                        <p>Lorem ipsum dolor</p>
+                        <h3>ONLY $40.00</h3>
+                    </div>
+                    <div class="offer-view">
+                        <img src="images/view2.png">
+                        <p>Lorem ipsum dolor</p>
+                        <h3>ONLY $40.00</h3>
+                    </div>
+                    <div class="offer-view">
+                        <img src="images/view3.png">
+                        <p>Lorem ipsum dolor</p>
+                        <h3>ONLY $40.00</h3>
+                    </div>
+                    <div class="offer-view">
+                        <img src="images/view4.png">
+                        <p>Lorem ipsum dolor</p>
+                        <h3>ONLY $40.00</h3>
+                    </div>
+                </div>
+            </main>
+            <div style="clear: both"></div>
+            <footer>
+                <div id="footer_top">
+                    <p>FOLLOW US ON</p>
+                    <ul>
+                        <li><a href="#"><img src="images/fb.png"></a></li>
+                        <li><a href="#"><img src="images/twitter.png"></a></li>
+                        <li><a href="#"><img src="images/googleplus.png"></a></li>
+                        <li><a href="#"><img src="images/a.png"></a></li>
+                        <li><a href="#"><img src="images/pin.png"></a></li>
+                    </ul>
+                    <form>
+                        <input type="text" placeholder="Enter your email to join newsletter ">
+                        <input type="submit" value="JION">
+                    </form>
+                </div>
+                <div id="footer_bottom">
+                    <div id="customer_care">
+                        <h3>customer center</h3>
+                        <ul>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="">How To Buy</a></li>
+                            <li><a href="">Delivery</a></li>
+                        </ul>
+                    </div>
+                    <div id="about_us">
+                        <h3>about us</h3>
+                        <ul>
+                            <li><a href="#">Our Stories</a></li>
+                            <li><a href="#">Press</a></li>
+                            <li><a href="">Career</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div id="my_account">
+                        <h3>my account</h3>
+                        <ul>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">My Cart</a></li>
+                            <li><a href="">Order History</a></li>
+                            <li><a href="">Payment</a></li>
+                        </ul>
+                    </div>
+                    <div id="our_stories">
+                        <h3>our stories</h3>
+                        <a href="#">SOLO</a>
+                        <a href="#">BOGOR</a>
+                        <ul>
+                            <li><a href="#">Okara, Punjab, Pkaitsna</a></li>
+                            <li><a href="#">0300-1234567</a></li>
+                            <li><a href="">ajmalhassan651@gmail.com</a></li>
+                            <!--<li><a href="">aa</a></li>-->
+                        </ul>
+                    </div>
+                    <p>All copy rights.&copy; 2018</p>
+                    <p>Term & Condition</p>
+                    <p>Privacy Policy</p>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
